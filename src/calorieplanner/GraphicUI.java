c51e3a09f04794e0028a4a02ca7cc3515b9cf68f
Joschua <joschua.info@web.de>
2026-03-27 15:53:53 +0100

    style: center everything - add icon

--- src/calorieplanner/GraphicUI.java
+++ src/calorieplanner/GraphicUI.java
@@ -13,6 +13,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 
 
@@ -27,6 +28,8 @@ public class GraphicUI extends Application{
 		
 		Label outputName = new Label();
 		
+		Label placeHolder = new Label();
+		
 		enterName.setOnAction(e -> {
 			String text1 = insertName.getText();
 			outputName.setText("Hallo " + text1 + ".");
@@ -36,14 +39,16 @@ public class GraphicUI extends Application{
 		insertName.setOnAction(e -> enterName.fire());
 		
 		HBox rootName = new HBox(1, insertName, enterName);
+		rootName.setAlignment(Pos.CENTER);
 		
-		VBox root = new VBox(10,outputName, rootName);
+		VBox root = new VBox(10,outputName, rootName, placeHolder);
 		root.setPadding(new Insets(10));
+		root.setAlignment(Pos.CENTER);
 		
 		Scene scene = new Scene(root, 300, 200);
 		stage.setTitle("Kalorienplaner");
 		stage.setScene(scene);
-//		stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
+		stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
 		
 		stage.show();
 
